be2a2cc1358976af552063022f40902c47cb7569
asareiko <asareiko@moog.com>
2024-09-21 22:15:09 +0300

    Database package dates migrations

---
+++ WeightScale.DataAccessLayer/Migrations/202409211727349_PackageDates.cs
@@ -0,0 +1,20 @@
+﻿namespace WeightScale.DataAccessLayer.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class PackageDates : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Packages", "FullPackageDate", c => c.DateTime(nullable: true));
+            AddColumn("dbo.Packages", "EmptyPackageDate", c => c.DateTime(nullable: true));
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.Packages", "EmptyPackageDate");
+            DropColumn("dbo.Packages", "FullPackageDate");
+        }
+    }
+}
